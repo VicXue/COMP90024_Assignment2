e7d65a4526d1d6979e68f71afbbb66bbb3ba2b0b
VicXue <xuezeyang@gmail.com>
2023-04-14 19:15:41 +1000

    feat: add ansible playbook
    for handling openstack provision, docker, and couchdb

---
+++ playbook/run-mrc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ansible-galaxy collection install openstack.cloud:1.10.0
+
+# . ./unimelb-comp90024-2023-grp-3-openrc.sh; ansible-playbook --ask-become-pass -i inventory/inventory.ini playbook.yaml 
+. ./unimelb-comp90024-2023-grp-3-openrc.sh; ansible-playbook -i inventory/inventory.ini playbook.yaml -vvv
