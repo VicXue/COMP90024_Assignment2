ae2f7a1ded06e9ef22037db3b32ce082b90b1498
VicXue <xuezeyang@gmail.com>
2023-04-18 23:01:37 +1000

    feat: update harvester

--- playbook/run-mrc.sh
+++ playbook/run-mrc.sh
@@ -3,4 +3,4 @@
 ansible-galaxy collection install openstack.cloud:1.10.0
 
 # . ./unimelb-comp90024-2023-grp-3-openrc.sh; ansible-playbook --ask-become-pass -i inventory/inventory.ini playbook.yaml 
-. ./unimelb-comp90024-2023-grp-3-openrc.sh; ansible-playbook -i inventory/inventory.ini playbook.yaml -vvv
+. ./unimelb-comp90024-2023-grp-3-openrc.sh; ansible-playbook -i inventory/inventory.ini playbook.yaml -vvv --tags "local,harvester"
