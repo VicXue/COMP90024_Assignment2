97f00481513d5a38c4df64d2d17fa7f3c3dcab5b
Quanchi Chen <quanchic@student.unimelb.edu.au>
2023-04-29 13:49:18 +1000

    Refactor harvester/main.py

--- harvester/main.py
+++ harvester/main.py
@@ -1,68 +1,61 @@
 import json
+import os
+
 import couchdb
-#import requests
 from mastodon import Mastodon, StreamListener
-import pandas as pd
+
 import harvester_util
-import os 
 
-
-
-#setup basic informatio for couchdb
+# Grab the admin account for the CouchDB Cluster.
 couchdb_admin = os.environ.get("DB_USER")
 couchdb_password = os.environ.get("DB_PASSWORD")
 
-#setup basic informatio for mastodon
+# Grab the URL and access token for the Mastodon server.
 mastodon_server = os.environ.get("MASTODON_SERVER")
 mastodon_token = os.environ.get("MASTODON_TOKEN")
 
+# Initialise a CouchDB server.
 couchdb_url = f'http://{couchdb_admin}:{couchdb_password}@172.26.134.0:5984/'
 couch = couchdb.Server(couchdb_url)
 
-#two db for two different contents
+# A list consisting of the names of the two databases employed in the project
 db_name_ls = ['mental_disabled_db', 'non_mental_disabled_db']
 
-print(f"Scraping Mastodon Server: {mastodon_server}")
 
 def main():
-    create_database()
+    create_databases()
+
     m = Mastodon(api_base_url=mastodon_server,
                  access_token=mastodon_token)  # Johnny's tocken.
+    print(f"Harvesting Mastodon Server: {mastodon_server}")
     m.stream_public(Listener())
 
 
-def create_database():
+def create_databases():
     """
-    Create a database in the CouchDB Cluster if it is not exits
+    Create two databases in the CouchDB Cluster if they do not exist.
     """
     for db in db_name_ls:
         if db not in couch:
-            db = couch.create(db)
-        else:
-            db = couch[db]
+            couch.create(db)
 
 
 class Listener(StreamListener):
     def on_update(self, status):
-        """
-        Assume that the database named toots exists in CouchDB.
-        Store a toot in CouchDB by sending a POST request to CouchDB.
-        Each toot is a document in that database.
-        """
-
         mental_db = couch[db_name_ls[0]]
         non_mental_db = couch[db_name_ls[1]]
 
         raw_data = json.dumps(status, sort_keys=True, default=str)
 
-        #process the raw toots here.
+        # Process the raw toots.
         processed_data = harvester_util.processed_fuction(json.loads(raw_data))
+
         if processed_data[0] == 0:
             doc_id, doc_rev = non_mental_db.save(json.loads(processed_data[1]))
-            print(f'Document saved with ID: {doc_id} and revision: {doc_rev}', 'in non_mental_db')
+            print(f'Document Saved [_id: {doc_id}, _rev: {doc_rev}]', 'in non_mental_db')
         else:
             doc_id, doc_rev = mental_db.save(json.loads(processed_data[1]))
-            print(f'Document saved with ID: {doc_id} and revision: {doc_rev}', 'in mental_db')
+            print(f'Document Saved [_id: {doc_id}, _rev: {doc_rev}]', 'in mental_db')
 
 
 if __name__ == "__main__":
