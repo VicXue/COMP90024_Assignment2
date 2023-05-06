728ad22bfe46ef1538e85477cf8968af2b489ab6
muuuuu <zmu1@student.unimelb.edu.au>
2023-05-06 23:45:30 +1000

    Line chart finished

--- frontend/src/components/Line/Line.js
+++ frontend/src/components/Line/Line.js
@@ -10,7 +10,7 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 
 ChartJS.register(
   CategoryScale,
@@ -30,32 +30,85 @@ export const options = {
     },
     title: {
       display: true,
-      text: "Chart.js Line Chart",
+      font: { size: 28 },
+      text: "Recurrent expenditure hisotry in mental health in VIC",
     },
   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+const labels = [
+  "2010-2011",
+  "2011-2012",
+  "2012-2013",
+  "2013-2014",
+  "2014-2015",
+  "2015-2016",
+  "2017-2018",
+  "2018-2019",
+  "2019-2020",
+  "2020-2021",
+];
 
 export const data = {
   labels,
   datasets: [
     {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      label: "Public psychiatric hospitals",
+      data: [
+        42208, 40821, 40483, 44445, 50024, 47502, 48593, 58459, 66707, 77588,
+        82698,
+      ],
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      label: "Specialised psychiatric units or wards in public acute hospitals",
+      data: [
+        271298, 274569, 286299, 303415, 320307, 342520, 385505, 443953, 494743,
+        512589, 526368,
+      ],
       borderColor: "rgb(53, 162, 235)",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+    {
+      label: "Community mental health care services",
+      data: [
+        368771, 394360, 401606, 426146, 424055, 443579, 471476, 583704, 597063,
+        651960, 706545,
+      ],
+      borderColor: "rgb(228, 25, 167)",
+      backgroundColor: "rgba(228, 25, 167, 0.5)",
+    },
+    {
+      label: "Residential mental health services",
+      data: [
+        164361, 164144, 172086, 190790, 189861, 187579, 192786, 204492, 228836,
+        231965, 230766,
+      ],
+      borderColor: "rgb(212, 219, 19)",
+      backgroundColor: "rgba(212, 219, 19, 0.5)",
+    },
+    {
+      label: "Grants to non-government organisations",
+      data: [
+        80406, 83643, 89429, 101296, 106761, 110883, 113882, 116711, 108982,
+        60163, 97983,
+      ],
+      borderColor: "rgb(72, 11, 216)",
+      backgroundColor: "rgba(72, 11, 216, 0.5)",
+    },
+    {
+      label: "Other indirect expenditure",
+      data: [
+        58336, 56086, 56387, 58814, 68022, 71005, 75017, 91811, 99023, 119558,
+        148536,
+      ],
+      borderColor: "rgb(8, 196, 102)",
+      backgroundColor: "rgba(8, 196, 102, 0.5)",
     },
   ],
 };
 
 export default function LineChart() {
-  console.log(labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })));
-  return <Line options={options} data={data} />;
+  return <Line className="line-chart" options={options} data={data} />;
 }
