b04882912520eddf9deeb62af0201137a54d01dd
muuuuu <zmu1@student.unimelb.edu.au>
2023-05-16 01:09:51 +1000

    fetch from backend

--- frontend/src/components/SUDOLineChart/LineChart.js
+++ frontend/src/components/SUDOLineChart/LineChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -40,99 +40,94 @@ export const options = {
       offset: 0,
       formatter: (value, ctx) => {
         return (value / 1000).toFixed(0) + "K";
-        // if (value > 0.04) {
-        //   return (value * 100 + "%").toString();
-        // } else {
-        //   return "";
-        // }
       },
     },
   },
 };
 
-const labels = [
-  "2010-2011",
-  "2011-2012",
-  "2012-2013",
-  "2013-2014",
-  "2014-2015",
-  "2015-2016",
-  "2017-2018",
-  "2018-2019",
-  "2019-2020",
-  "2020-2021",
-];
+function LineChart() {
+  const API_URL = process.env.REACT_APP_BACKEND_API_HOST;
+  const [vicData, setVicData] = useState(null);
+  const [chartData, setChartData] = useState(null);
+  const newLabels = [
+    "2010-2011",
+    "2011-2012",
+    "2012-2013",
+    "2013-2014",
+    "2014-2015",
+    "2015-2016",
+    "2017-2018",
+    "2018-2019",
+    "2019-2020",
+    "2020-2021",
+  ];
 
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Public psychiatric hospitals",
-      data: [
-        42208, 40821, 40483, 44445, 50024, 47502, 48593, 58459, 66707, 77588,
-        82698,
-      ],
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-      hidden: true,
-    },
-    {
-      label: "Specialised psychiatric units or wards in public acute hospitals",
-      data: [
-        271298, 274569, 286299, 303415, 320307, 342520, 385505, 443953, 494743,
-        512589, 526368,
-      ],
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-      hidden: true,
-    },
-    {
-      label: "Community mental health care services",
-      data: [
-        368771, 394360, 401606, 426146, 424055, 443579, 471476, 583704, 597063,
-        651960, 706545,
-      ],
-      borderColor: "rgb(228, 25, 167)",
-      backgroundColor: "rgba(228, 25, 167, 0.5)",
-      hidden: true,
-    },
-    {
-      label: "Residential mental health services",
-      data: [
-        164361, 164144, 172086, 190790, 189861, 187579, 192786, 204492, 228836,
-        231965, 230766,
-      ],
-      borderColor: "rgb(212, 219, 19)",
-      backgroundColor: "rgba(212, 219, 19, 0.5)",
-      hidden: true,
-    },
-    {
-      label: "Grants to non-government organisations",
-      data: [
-        80406, 83643, 89429, 101296, 106761, 110883, 113882, 116711, 108982,
-        60163, 97983,
-      ],
-      borderColor: "rgb(72, 11, 216)",
-      backgroundColor: "rgba(72, 11, 216, 0.5)",
-      hidden: true,
-    },
-    {
-      label: "Other indirect expenditure",
-      data: [
-        58336, 56086, 56387, 58814, 68022, 71005, 75017, 91811, 99023, 119558,
-        148536,
-      ],
-      borderColor: "rgb(8, 196, 102)",
-      backgroundColor: "rgba(8, 196, 102, 0.5)",
-      hidden: true,
-    },
-  ],
-};
+  useEffect(() => {
+    const fetchJSON = async () => {
+      try {
+        const response = await fetch(
+          API_URL + ":8080/api/v1/analysis/victoria-expenses"
+        );
+        const data = await response.json();
 
-export default function LineChart() {
+        setVicData(data.data.expenses);
+      } catch (error) {
+        console.error("Error fetching GeoJSON data:", error);
+      }
+    };
+
+    fetchJSON();
+  }, []);
+
+  useEffect(() => {
+    if (vicData) {
+      let datasets = [];
+
+      vicData.map((expense) => {
+        let rgb1 = Math.floor(Math.random() * 255).toString();
+        let rgb2 = Math.floor(Math.random() * 255).toString();
+        let rgb3 = Math.floor(Math.random() * 255).toString();
+
+        let dataset = {
+          label: expense["Expenditure section"],
+          data: [
+            expense["2010-11"].replace(",", ""),
+            expense["2011-12"].replace(",", ""),
+            expense["2012-13"].replace(",", ""),
+            expense["2013-14"].replace(",", ""),
+            expense["2014-15"].replace(",", ""),
+            expense["2015-16"].replace(",", ""),
+            expense["2016-17"].replace(",", ""),
+            expense["2017-18"].replace(",", ""),
+            expense["2018-19"].replace(",", ""),
+            expense["2019-20"].replace(",", ""),
+            expense["2020-21"].replace(",", ""),
+          ],
+          borderColor: `rgb(${rgb1}, ${rgb2}, ${rgb3})`,
+          backgroundColor: `rgb(${rgb1}, ${rgb2}, ${rgb3}, 0.5)`,
+          hidden: true,
+        };
+
+        datasets.push(dataset);
+
+        return dataset;
+      });
+
+      datasets.pop(-1);
+      setChartData({ labels: newLabels, datasets: datasets });
+    }
+  }, [vicData]);
+
   return (
+    // <div className="sudo-chart">
+    //   <Line className="line-chart" options={options} data={chartData} />
+    // </div>
     <div className="sudo-chart">
-      <Line className="line-chart" options={options} data={data} />
+      {chartData ? (
+        <Line className="line-chart" options={options} data={chartData} />
+      ) : null}
     </div>
   );
 }
+
+export default LineChart;
